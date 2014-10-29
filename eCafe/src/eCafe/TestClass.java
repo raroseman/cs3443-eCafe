a056be8ecf6f4877276ffe93f59aac5285ce284a
mikeschap <mikeschappel@gmail.com>
2014-10-29 14:07:24 -0500

    Class modification test

--- eCafe/src/eCafe/TestClass.java
+++ eCafe/src/eCafe/TestClass.java
@@ -4,9 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		char[] array = { 'H', 'E', 'L', 'L', 'O' };
-
-		System.out
-				.println("This is a test class.\nIf you are able to pull and run this class,\nthe test was successful.");
+		System.out.println("This is a test class.\nIf you are able to pull and run this class,\nthe test was successful.");
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i]);
 		}
