44fdff008d5bb9bf0d69c5bcfc44a3407af67a1d
eqn397 <eqn397@hotmail.com>
2014-11-21 10:40:18 -0600

    Ingredient Class

--- eCafe/src/Model/Ingredient.java
+++ eCafe/src/Model/Ingredient.java
@@ -1,5 +1,29 @@
 package Model;
 
 public class Ingredient {
+	
+	private float cost;
+	
+	private String name;
+	
+	public Ingredient(float cost, String name){
+		this.cost = cost;
+		this.name = name;
+	}
+	
+	public float getCost(){
+		return cost;
+	}
+	
+	public void setCost(float cost){
+		this.cost = cost;
+	}
+	
+	public String getName(){
+		return name;
+	}
 
+	public void setName(String name){
+		this.name = name;
+	}
 }
--- eCafe/src/Model/Table.java
+++ eCafe/src/Model/Table.java
@@ -1,5 +1,12 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Table {
 
+	public boolean available;
+	
+	private ArrayList<Order> orders = new ArrayList<Order>();
+		
+	
 }
