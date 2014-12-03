38c557882550d2200bd6d758b263df99e7225a41
eqn397 <eqn397@hotmail.com>
2014-12-03 12:43:06 -0600

    Account and Customer modifications

--- eCafe/src/Model/Account.java
+++ eCafe/src/Model/Account.java
@@ -3,28 +3,53 @@ package Model;
 public class Account {
 	private double cash;
 
+	/**
+	 * Constructor to initialize the 
+	 * account to 0
+	 */
 	public Account() {
 		cash = 0;
 	}
 
+	/**
+	 * Constructor to initialize it to
+	 * cash
+	 * @param cash
+	 */
 	public Account(double cash) {
 		this.cash = cash;
 	}
 
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~GETTERS/SETTERS
+	/**
+	 * Gets the cash
+	 * @return cash
+	 */
 	public double getCash() {
 		return cash;
 	}
 
+	/**
+	 * Sets the cash amount
+	 * @param cash
+	 */
 	public void setCash(double cash) {
 		this.cash = cash;
 	}
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~GETTERS/SETTERS
 
+	/**
+	 * Cash in
+	 * @param price
+	 */
 	public void orderPaid(double price) {
 		this.cash += price;
 	}
 
+	/**
+	 * Cash out
+	 * @param cost
+	 */
 	public void buyIng(double cost) {
 		this.cash -= cost;
 	}
--- eCafe/src/Model/Customer.java
+++ eCafe/src/Model/Customer.java
@@ -1,5 +1,17 @@
 package Model;
 
 public class Customer {
+	private String name;
 	
+	public Customer(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
 }
