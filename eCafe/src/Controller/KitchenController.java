b252cb75725020bb0a95a162f4d6fc37d1af8816
mikeschap <mikeschappel@gmail.com>
2014-12-06 13:38:39 -0600

    Problem fix test

---
+++ eCafe/src/Controller/KitchenController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class KitchenController implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
