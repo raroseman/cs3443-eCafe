1f18fdae402aa1b989d7f066ac830940737757f5
Ryan Roseman <ryanroseman@yahoo.com>
2014-12-08 20:09:57 -0600

    Working part of kitchenController class. Still implementing the rest.

--- eCafe/src/Controller/KitchenController.java
+++ eCafe/src/Controller/KitchenController.java
@@ -2,13 +2,23 @@ package Controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.PriorityQueue;
+
+import View.KitchenView;
 
 public class KitchenController implements ActionListener {
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
+		private KitchenView view;
+		private PriorityQueue kitchenQueue = new PriorityQueue();
+		
+		public KitchenController(KitchenView view) {
+			this.view = view;
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			String command = e.getActionCommand();
+			if (command == "Serve")
+				//view.populateReadyField("meow");
+		}
 }
+
+
